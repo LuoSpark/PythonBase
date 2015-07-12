91610f23e4c91e68e4e6a9bee70b3645fb832d5a
LuoSpark <651916010@qq.com>
2015-07-12 16:37:35 +0800

    例行保存

--- test01.py
+++ test01.py
@@ -1,9 +1,22 @@
-ll = [1,2]
+##ll = [1,2]
+##
+##def fun():
+##    ll = [3,4]
+##    print ll
+##
+##fun()
+##
+##print ll
 
-def fun():
-    ll = [3,4]
-    print ll
 
-fun()
+def count():
+    i = 0
+    fs = []
+    for i in range(1,4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
 
-print ll
+f1,f2,f3 = count()
+print f1(),f2(),f3()
